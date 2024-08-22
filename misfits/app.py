1fc0b8f92ba84f43f57a7f5d224e551f8b2b11c9
peppedilillo <peppedilillo@gmail.com>
2024-08-22 17:22:12 +0200

    first commit

---
+++ misfits/app.py
@@ -0,0 +1,26 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+
+
+class MisfitsApp(App):
+    """A Textual app to manage stopwatches."""
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+
+def main():
+    app = MisfitsApp()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
